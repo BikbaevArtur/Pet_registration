321d1bb5ab5b9ccf68a95bbdd4e88fe4524e39ce
arturbikbaev <bigbyar4i@icloud.com>
2024-02-08 16:00:56 +0500

    Обработка исключения пунктов меню

--- src/main/java/ru/bikbaev/userInterface/InterfaceMenu.java
+++ src/main/java/ru/bikbaev/userInterface/InterfaceMenu.java
@@ -3,12 +3,13 @@ package ru.bikbaev.userInterface;
 import ru.bikbaev.controller.PetController;
 import ru.bikbaev.model.TypePet;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InterfaceMenu {
 
-    private PetController petController;
-    private InterfaceView interfaceView;
+    private final PetController petController;
+    private final InterfaceView interfaceView;
 
     private final Scanner scanner;
 
@@ -21,31 +22,44 @@ public class InterfaceMenu {
     public void start() {
         boolean flag = true;
         while (flag) {
-            System.out.println("Выберите пункты меню:");
-            System.out.println("0 Выход");
-            System.out.println("1 Добавить нового  питомца");
-            System.out.println("2 Удалить питомца");
-            System.out.println("3 Выбрать питомца");
-            System.out.println("4 Вывести на экран всех питомцев");
+
+            try {
+
+                System.out.println("Выберите пункты меню:");
+                System.out.println("0 Выход");
+                System.out.println("1 Добавить нового  питомца");
+                System.out.println("2 Удалить питомца");
+                System.out.println("3 Выбрать питомца");
+                System.out.println("4 Вывести на экран всех питомцев");
 
 
-            int button = scanner.nextInt();
-            switch (button) {
-                case 0:
-                    flag = false;
-                    break;
-                case 1:
-                    addMenu();
-                    break;
-                case 2:
-                    petController.deletePet(petController.getPetIndex(interfaceView.setName()));
-                    break;
-                case 3:
-                    petMenu();
-                    break;
-                case 4:
-                    interfaceView.printAll(petController.getPets());
-                    break;
+                int button = scanner.nextInt();
+
+
+                switch (button) {
+                    case 0:
+                        flag = false;
+                        break;
+                    case 1:
+                        addMenu();
+                        break;
+                    case 2:
+                        petController.deletePet(petController.getPetIndex(interfaceView.setName()));
+                        break;
+                    case 3:
+                        petMenu();
+                        break;
+                    case 4:
+                        interfaceView.printAll(petController.getPets());
+                        break;
+                    default:
+                        System.out.println("\nВыберите корректный пункт меню:");
+                }
+            }
+            catch (InputMismatchException ex){
+                System.out.println("\u001B[31m"+"Ошибка: Некорректный ввод"+ "\u001B[0m");
+                System.out.println("\u001B[32m"+"Введите числовой номер меню:" + "\u001B[0m");
+                scanner.next();
             }
         }
 
@@ -57,50 +71,73 @@ public class InterfaceMenu {
         boolean flag = true;
 
         while (flag) {
-            System.out.println("0 Назад");
-            System.out.println("1 Добавить Кота");
-            System.out.println("2 Добавить Собаку");
-            System.out.println("3 Добавить хомяка");
-            int button = scanner.nextInt();
-            switch (button) {
-                case 0:
-                    flag = false;
-                    start();
-                    break;
-                case 1:
-                    petController.creatPet(TypePet.Cat);
-                    break;
-                case 2:
-                    petController.creatPet(TypePet.Dog);
-                    break;
-                case 3:
-                    petController.creatPet(TypePet.Hamster);
-                    break;
+            try {
+
+                System.out.println("0 Назад");
+                System.out.println("1 Добавить Кота");
+                System.out.println("2 Добавить Собаку");
+                System.out.println("3 Добавить хомяка");
+                int button = scanner.nextInt();
+                if (button < 0 || button > 3) {
+                    System.out.println("Выберите корректный пункт в меню");
+                }
+                switch (button) {
+                    case 0:
+                        flag = false;
+                        start();
+                        break;
+                    case 1:
+                        petController.creatPet(TypePet.Cat);
+                        break;
+                    case 2:
+                        petController.creatPet(TypePet.Dog);
+                        break;
+                    case 3:
+                        petController.creatPet(TypePet.Hamster);
+                        break;
+                    default:
+                        System.out.println("\nВыберите корректный пункт меню:");
+                }
+            }
+            catch (InputMismatchException ex){
+                System.out.println("\u001B[31m"+"Ошибка: Некорректный ввод"+ "\u001B[0m");
+                System.out.println("\u001B[32m"+"Введите числовой номер меню:" + "\u001B[0m");
+                scanner.next();
             }
         }
 
     }
 
     private void petMenu() {
+
         boolean flag = true;
-
         while (flag) {
+            try {
 
-            System.out.println("0 Назад");
-            System.out.println("1 Найти питомца");
-            System.out.println("2 Обучить питомца командам");
-            int button = scanner.nextInt();
-            switch (button) {
-                case 0:
-                    flag = false;
-                    start();
-                    break;
-                case 1:
-                    System.out.println(petController.getPet());
-                    break;
-                case 2:
-                    System.out.println("Выберите питомца: ");
-                    petController.trainPet();
+                System.out.println("0 Назад");
+                System.out.println("1 Найти питомца");
+                System.out.println("2 Обучить питомца командам");
+                int button = scanner.nextInt();
+
+                switch (button) {
+                    case 0:
+                        flag = false;
+                        start();
+                        break;
+                    case 1:
+                        System.out.println(petController.getPet());
+                        break;
+                    case 2:
+                        System.out.println("Выберите питомца: ");
+                        petController.trainPet();
+                    default:
+                        System.out.println("\nВыберите корректный пункт меню:");
+                }
+            }
+            catch (InputMismatchException ex ){
+                System.out.println("\u001B[31m"+"Ошибка: Некорректный ввод"+ "\u001B[0m");
+                System.out.println("\u001B[32m"+"Введите числовой номер меню:" + "\u001B[0m");
+                scanner.next();
             }
         }
     }
